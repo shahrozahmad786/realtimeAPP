42ba1ad3ffc063c11eff790db0a65b5f77ac61cf
shahroz <shahrozahmad786@gmail.com>
2019-03-09 15:35:12 +0500

    jwt course

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,18 @@
 <?php
 
 Route::apiResource('/question','QuestionController');
+
+
+Route::group([
+
+      'middleware' => 'api',
+      'prefix' => 'auth'
+  
+  ], function ($router) {
+  
+      Route::post('login', 'AuthController@login');
+      Route::post('logout', 'AuthController@logout');
+      Route::post('refresh', 'AuthController@refresh');
+      Route::post('me', 'AuthController@me');
+  
+  });
